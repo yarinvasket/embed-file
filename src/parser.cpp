fbc7aecf699f5ffdfb92f4b6acf2703d23c3620c
vasker <yarinvasket@gmail.com>
2022-03-03 23:30:42 +0200

    apperently std::byte sucks

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,6 +2,8 @@
 #include <stdexcept>
 #include <string>
 
+using byte = unsigned char;
+
 int main(int argc, char *argv[]) {
 	if (argc < 3) {
 		throw std::invalid_argument("Too few arguments");
@@ -12,18 +14,18 @@ int main(int argc, char *argv[]) {
 	f.close();
 	f = std::ifstream(argv[1], std::ios::binary);
 
-	std::byte* buffer = new std::byte[size];
+	byte* buffer = new byte[size];
 	f.read((char *)buffer, size);
 	f.close();
 
 	std::string toWrite;
 	for (size_t i = 0; i < size; i++) {
-		toWrite += std::to_string((unsigned int)(buffer[i])) + ',';
+		toWrite += std::to_string(buffer[i]) + ',';
 	}
+	delete[] buffer;
 	toWrite.pop_back();
 	auto buffer2 = toWrite.data();
 	auto of = std::ofstream(argv[2], std::ios::binary);
 	of.write(buffer2, toWrite.length());
 	of.close();
-	delete[] buffer;
 }
